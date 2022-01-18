65aa659a2d575f90eb274e039824e5d4c0a01b02
Rifleman <Rifleman@EVERSMILE>
2022-01-18 15:55:42 +0100

    Add project files.

---
+++ Quartermaster/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Quartermaster
+{
+    partial class Quartermaster
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Quartermaster));
+            this.numLaptop2New = new System.Windows.Forms.NumericUpDown();
+            this.numLaptop2Old = new System.Windows.Forms.NumericUpDown();
+            this.numLaptop1Old = new System.Windows.Forms.NumericUpDown();
+            this.numLaptop1New = new System.Windows.Forms.NumericUpDown();
+            this.num27inch = new System.Windows.Forms.NumericUpDown();
+            this.num24inch = new System.Windows.Forms.NumericUpDown();
+            this.numKBMouse = new System.Windows.Forms.NumericUpDown();
+            this.numDock = new System.Windows.Forms.NumericUpDown();
+            this.numHeadset = new System.Windows.Forms.NumericUpDown();
+            this.lblLaptop1 = new System.Windows.Forms.Label();
+            this.lblLaptop2 = new System.Windows.Forms.Label();
+            this.lblDisplay27 = new System.Windows.Forms.Label();
+            this.lblDisplay24 = new System.Windows.Forms.Label();
+            this.lblKBMouse = new System.Windows.Forms.Label();
+            this.lblDock = new System.Windows.Forms.Label();
+            this.lblHeadset = new System.Windows.Forms.Label();
+            this.lblHiddenAuthor = new System.Windows.Forms.Label();
+            this.dialogDataPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkAllowEdit = new System.Windows.Forms.CheckBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numLaptop2New)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLaptop2Old)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLaptop1Old)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLaptop1New)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num27inch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num24inch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numKBMouse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeadset)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numLaptop2New
+            // 
+            this.numLaptop2New.Enabled = false;
+            this.numLaptop2New.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numLaptop2New.Location = new System.Drawing.Point(36, 149);
+            this.numLaptop2New.Name = "numLaptop2New";
+            this.numLaptop2New.Size = new System.Drawing.Size(58, 33);
+            this.numLaptop2New.TabIndex = 6;
+            // 
+            // numLaptop2Old
+            // 
+            this.numLaptop2Old.Enabled = false;
+            this.numLaptop2Old.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numLaptop2Old.Location = new System.Drawing.Point(36, 183);
+            this.numLaptop2Old.Name = "numLaptop2Old";
+            this.numLaptop2Old.Size = new System.Drawing.Size(58, 33);
+            this.numLaptop2Old.TabIndex = 7;
+            // 
+            // numLaptop1Old
+            // 
+            this.numLaptop1Old.Enabled = false;
+            this.numLaptop1Old.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numLaptop1Old.Location = new System.Drawing.Point(36, 85);
+            this.numLaptop1Old.Name = "numLaptop1Old";
+            this.numLaptop1Old.Size = new System.Drawing.Size(58, 33);
+            this.numLaptop1Old.TabIndex = 5;
+            // 
+            // numLaptop1New
+            // 
+            this.numLaptop1New.Enabled = false;
+            this.numLaptop1New.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numLaptop1New.Location = new System.Drawing.Point(36, 51);
+            this.numLaptop1New.Name = "numLaptop1New";
+            this.numLaptop1New.Size = new System.Drawing.Size(58, 33);
+            this.numLaptop1New.TabIndex = 4;
+            // 
+            // num27inch
+            // 
+            this.num27inch.Enabled = false;
+            this.num27inch.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num27inch.Location = new System.Drawing.Point(142, 51);
+            this.num27inch.Name = "num27inch";
+            this.num27inch.Size = new System.Drawing.Size(58, 33);
+            this.num27inch.TabIndex = 9;
+            // 
+            // num24inch
+            // 
+            this.num24inch.Enabled = false;
+            this.num24inch.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num24inch.Location = new System.Drawing.Point(141, 119);
+            this.num24inch.Name = "num24inch";
+            this.num24inch.Size = new System.Drawing.Size(58, 33);
+            this.num24inch.TabIndex = 10;
+            // 
+            // numKBMouse
+            // 
+            this.numKBMouse.Enabled = false;
+            this.numKBMouse.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numKBMouse.Location = new System.Drawing.Point(142, 246);
+            this.numKBMouse.Name = "numKBMouse";
+            this.numKBMouse.Size = new System.Drawing.Size(58, 33);
+            this.numKBMouse.TabIndex = 12;
+            // 
+            // numDock
+            // 
+            this.numDock.Enabled = false;
+            this.numDock.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numDock.Location = new System.Drawing.Point(141, 183);
+            this.numDock.Name = "numDock";
+            this.numDock.Size = new System.Drawing.Size(58, 33);
+            this.numDock.TabIndex = 11;
+            // 
+            // numHeadset
+            // 
+            this.numHeadset.Enabled = false;
+            this.numHeadset.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numHeadset.Location = new System.Drawing.Point(36, 246);
+            this.numHeadset.Name = "numHeadset";
+            this.numHeadset.Size = new System.Drawing.Size(58, 33);
+            this.numHeadset.TabIndex = 8;
+            // 
+            // lblLaptop1
+            // 
+            this.lblLaptop1.AutoSize = true;
+            this.lblLaptop1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLaptop1.Location = new System.Drawing.Point(26, 121);
+            this.lblLaptop1.Name = "lblLaptop1";
+            this.lblLaptop1.Size = new System.Drawing.Size(80, 25);
+            this.lblLaptop1.TabIndex = 0;
+            this.lblLaptop1.Text = "Latitude";
+            // 
+            // lblLaptop2
+            // 
+            this.lblLaptop2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLaptop2.Location = new System.Drawing.Point(22, 23);
+            this.lblLaptop2.Name = "lblLaptop2";
+            this.lblLaptop2.Size = new System.Drawing.Size(89, 25);
+            this.lblLaptop2.TabIndex = 0;
+            this.lblLaptop2.Text = "Precision";
+            // 
+            // lblDisplay27
+            // 
+            this.lblDisplay27.AutoSize = true;
+            this.lblDisplay27.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDisplay27.Location = new System.Drawing.Point(118, 23);
+            this.lblDisplay27.Name = "lblDisplay27";
+            this.lblDisplay27.Size = new System.Drawing.Size(103, 25);
+            this.lblDisplay27.TabIndex = 0;
+            this.lblDisplay27.Text = "27\" display";
+            // 
+            // lblDisplay24
+            // 
+            this.lblDisplay24.AutoSize = true;
+            this.lblDisplay24.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDisplay24.Location = new System.Drawing.Point(118, 91);
+            this.lblDisplay24.Name = "lblDisplay24";
+            this.lblDisplay24.Size = new System.Drawing.Size(103, 25);
+            this.lblDisplay24.TabIndex = 0;
+            this.lblDisplay24.Text = "24\" display";
+            // 
+            // lblKBMouse
+            // 
+            this.lblKBMouse.AutoSize = true;
+            this.lblKBMouse.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblKBMouse.Location = new System.Drawing.Point(124, 218);
+            this.lblKBMouse.Name = "lblKBMouse";
+            this.lblKBMouse.Size = new System.Drawing.Size(97, 25);
+            this.lblKBMouse.TabIndex = 0;
+            this.lblKBMouse.Text = "KB/mouse";
+            // 
+            // lblDock
+            // 
+            this.lblDock.AutoSize = true;
+            this.lblDock.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDock.Location = new System.Drawing.Point(141, 155);
+            this.lblDock.Name = "lblDock";
+            this.lblDock.Size = new System.Drawing.Size(54, 25);
+            this.lblDock.TabIndex = 0;
+            this.lblDock.Text = "Dock";
+            // 
+            // lblHeadset
+            // 
+            this.lblHeadset.AutoSize = true;
+            this.lblHeadset.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHeadset.Location = new System.Drawing.Point(26, 218);
+            this.lblHeadset.Name = "lblHeadset";
+            this.lblHeadset.Size = new System.Drawing.Size(80, 25);
+            this.lblHeadset.TabIndex = 0;
+            this.lblHeadset.Text = "Headset";
+            // 
+            // lblHiddenAuthor
+            // 
+            this.lblHiddenAuthor.AutoSize = true;
+            this.lblHiddenAuthor.Location = new System.Drawing.Point(89, 399);
+            this.lblHiddenAuthor.Name = "lblHiddenAuthor";
+            this.lblHiddenAuthor.Size = new System.Drawing.Size(87, 15);
+            this.lblHiddenAuthor.TabIndex = 0;
+            this.lblHiddenAuthor.Text = "Version 0.2.1.15";
+            this.lblHiddenAuthor.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LblHiddenAuthor_MouseDoubleClick);
+            // 
+            // checkAllowEdit
+            // 
+            this.checkAllowEdit.AutoSize = true;
+            this.checkAllowEdit.Location = new System.Drawing.Point(71, 326);
+            this.checkAllowEdit.Name = "checkAllowEdit";
+            this.checkAllowEdit.Size = new System.Drawing.Size(117, 19);
+            this.checkAllowEdit.TabIndex = 3;
+            this.checkAllowEdit.Text = "Update inventory";
+            this.checkAllowEdit.UseVisualStyleBackColor = true;
+            this.checkAllowEdit.CheckedChanged += new System.EventHandler(this.CheckAllowEdit_CheckedChanged);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(48, 360);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Source...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(137, 360);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblHeadset);
+            this.groupBox1.Controls.Add(this.lblKBMouse);
+            this.groupBox1.Controls.Add(this.lblDock);
+            this.groupBox1.Controls.Add(this.lblDisplay27);
+            this.groupBox1.Controls.Add(this.lblDisplay24);
+            this.groupBox1.Controls.Add(this.lblLaptop2);
+            this.groupBox1.Controls.Add(this.lblLaptop1);
+            this.groupBox1.Controls.Add(this.numHeadset);
+            this.groupBox1.Controls.Add(this.numKBMouse);
+            this.groupBox1.Controls.Add(this.numDock);
+            this.groupBox1.Controls.Add(this.num27inch);
+            this.groupBox1.Controls.Add(this.num24inch);
+            this.groupBox1.Controls.Add(this.numLaptop1Old);
+            this.groupBox1.Controls.Add(this.numLaptop1New);
+            this.groupBox1.Controls.Add(this.numLaptop2Old);
+            this.groupBox1.Controls.Add(this.numLaptop2New);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(246, 296);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inventory";
+            // 
+            // Quartermaster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 421);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.checkAllowEdit);
+            this.Controls.Add(this.lblHiddenAuthor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Quartermaster";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quartermaster";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Quartermaster_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numLaptop2New)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLaptop2Old)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLaptop1Old)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLaptop1New)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num27inch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num24inch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numKBMouse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeadset)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NumericUpDown numLaptop2New;
+        private NumericUpDown numLaptop2Old;
+        private NumericUpDown numLaptop1Old;
+        private NumericUpDown numLaptop1New;
+        private NumericUpDown num27inch;
+        private NumericUpDown num24inch;
+        private NumericUpDown numKBMouse;
+        private NumericUpDown numDock;
+        private NumericUpDown numHeadset;
+        private Label lblLaptop1;
+        private Label lblLaptop2;
+        private Label lblDisplay27;
+        private Label lblDisplay24;
+        private Label lblKBMouse;
+        private Label lblDock;
+        private Label lblHeadset;
+        private Label lblHiddenAuthor;
+        private FolderBrowserDialog dialogDataPath;
+        private CheckBox checkAllowEdit;
+        private Button btnOpen;
+        private Button btnSave;
+        private GroupBox groupBox1;
+    }
+}
---
+++ Quartermaster/Form1.cs
@@ -0,0 +1,213 @@
+using System;
+using System.IO;
+using System.Media;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Quartermaster
+{
+    public partial class Quartermaster : Form
+    {
+        public Quartermaster()
+        {
+            InitializeComponent();
+        }
+
+        int laptop1New;
+        int laptop2New;
+        int laptop1Old;
+        int laptop2Old;
+        int dock;
+        int headset;
+        int kbMouse;
+        int twentyFourInch;
+        int twentySevenInch;
+        string ConfigFolderPath = "";
+        private const string ConfigFile = @"\QMconfig.txt";
+        string DataFolderPath = "";
+        private const string DataFile = @"\QMsource.xls";
+
+        private void AutoConfig()
+        {
+            ConfigFolderPath = Directory.GetCurrentDirectory();
+            if(!File.Exists(ConfigFolderPath + ConfigFile))
+            {
+                MessageBox.Show("Please select a folder to save your inventory data. You can change it later at any time.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                if (dialogDataPath.ShowDialog() == DialogResult.OK)
+                {
+                    DataFolderPath = dialogDataPath.SelectedPath;
+                }
+                using (StreamWriter sw = File.CreateText(ConfigFolderPath + ConfigFile))
+                {
+                    sw.WriteLine(DataFolderPath);
+                    if (File.Exists(DataFolderPath + DataFile))
+                    {
+                        ReadFromExcel();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                using (StreamReader sr = File.OpenText(ConfigFolderPath + ConfigFile))
+                {
+                    DataFolderPath = sr.ReadLine();
+                    ReadFromExcel();
+                }
+            }
+        }
+        private void WriteToExcel()
+        {
+            Excel.Application myexcelApplication = new Excel.Application();
+            if (myexcelApplication != null)
+            {
+                Excel.Workbook myexcelWorkbook = myexcelApplication.Workbooks.Add();
+                Excel.Worksheet myexcelWorksheet = (Excel.Worksheet)myexcelWorkbook.Sheets.Add();
+
+                laptop1New = Convert.ToInt32(numLaptop1New.Value);
+                myexcelWorksheet.Cells[1, 1] = laptop1New;
+                laptop1Old = Convert.ToInt32(numLaptop1Old.Value);
+                myexcelWorksheet.Cells[2, 1] = laptop1Old;
+                laptop2New = Convert.ToInt32(numLaptop2New.Value);
+                myexcelWorksheet.Cells[3, 1] = laptop2New;
+                laptop2Old = Convert.ToInt32(numLaptop2Old.Value);
+                myexcelWorksheet.Cells[4, 1] = laptop2Old;
+                dock = Convert.ToInt32(numDock.Value);
+                myexcelWorksheet.Cells[5, 1] = dock;
+                headset = Convert.ToInt32(numHeadset.Value);
+                myexcelWorksheet.Cells[6, 1] = headset;
+                kbMouse = Convert.ToInt32(numKBMouse.Value);
+                myexcelWorksheet.Cells[7, 1] = kbMouse;
+                twentyFourInch = Convert.ToInt32(num24inch.Value);
+                myexcelWorksheet.Cells[8, 1] = twentyFourInch;
+                twentySevenInch = Convert.ToInt32(num27inch.Value);
+                myexcelWorksheet.Cells[9, 1] = twentySevenInch;
+                              
+                myexcelApplication.ActiveWorkbook.SaveAs(@DataFolderPath + DataFile, Excel.XlFileFormat.xlWorkbookNormal);
+
+                myexcelWorkbook.Close();
+                myexcelApplication.Quit();
+            }
+        }
+
+        private void ReadFromExcel()
+        {
+            Excel.Application myexcelApplication = new Excel.Application();
+            if (myexcelApplication != null)
+            {
+                Excel.Workbook myexcelWorkbook = myexcelApplication.Workbooks.Open(@DataFolderPath + DataFile);
+                Excel.Worksheet myexcelWorksheet = (Excel.Worksheet)myexcelWorkbook.Sheets[1];
+
+                numLaptop1New.Value = Convert.ToInt32(myexcelWorksheet.Cells[1, 1].Value);
+                numLaptop1Old.Value = Convert.ToInt32(myexcelWorksheet.Cells[2, 1].Value);
+                numLaptop2New.Value = Convert.ToInt32(myexcelWorksheet.Cells[3, 1].Value);
+                numLaptop2Old.Value = Convert.ToInt32(myexcelWorksheet.Cells[4, 1].Value);
+                numDock.Value = Convert.ToInt32(myexcelWorksheet.Cells[5, 1].Value);
+                numHeadset.Value = Convert.ToInt32(myexcelWorksheet.Cells[6, 1].Value);
+                numKBMouse.Value = Convert.ToInt32(myexcelWorksheet.Cells[7, 1].Value);
+                num24inch.Value = Convert.ToInt32(myexcelWorksheet.Cells[8, 1].Value);
+                num27inch.Value = Convert.ToInt32(myexcelWorksheet.Cells[9, 1].Value);
+
+                myexcelWorkbook.Close();
+                myexcelApplication.Quit();
+            }
+        }
+
+        private static void PlayChime()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+            simpleSound.Play();
+        }
+
+        private static void PlayTada()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\tada.wav");
+            simpleSound.Play();
+        }
+
+        private void EnableNums()
+        {
+            numLaptop1New.Enabled = true;
+            numLaptop1Old.Enabled = true;
+            numLaptop2New.Enabled = true;
+            numLaptop2Old.Enabled = true;
+            num24inch.Enabled = true;
+            num27inch.Enabled = true;
+            numDock.Enabled = true;
+            numHeadset.Enabled = true;
+            numKBMouse.Enabled = true;
+        }
+
+        private void DisableNums()
+        {
+            numLaptop1New.Enabled = false;
+            numLaptop1Old.Enabled = false;
+            numLaptop2New.Enabled = false;
+            numLaptop2Old.Enabled = false;
+            num24inch.Enabled = false;
+            num27inch.Enabled = false;
+            numDock.Enabled = false;
+            numHeadset.Enabled = false;
+            numKBMouse.Enabled = false;
+        }
+
+        private void CheckAllowEdit_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkAllowEdit.Checked)
+            {
+                EnableNums();
+                if(btnSave.Enabled == false)
+                {
+                    btnSave.Enabled = true;
+                    btnSave.Text = "Save";
+                }
+            }
+            else
+            {
+                DisableNums();
+                btnSave.Enabled = false;
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            WriteToExcel();
+            PlayChime();
+            btnSave.Text = "Saved!";
+            btnSave.Enabled = false;
+            checkAllowEdit.Checked = false;
+        }
+
+        private void LblHiddenAuthor_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            PlayTada();
+            MessageBox.Show("       Proudly made by Rifleman in 2022", "Credits", MessageBoxButtons.OK);
+        }
+
+        private void Quartermaster_Load(object sender, EventArgs e)
+        {
+            AutoConfig();           
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            if(dialogDataPath.ShowDialog() == DialogResult.OK)
+            {
+                DataFolderPath = dialogDataPath.SelectedPath;
+
+                EnableNums();
+                ReadFromExcel();
+                Task.Delay(100);
+                DisableNums();
+
+                using (StreamWriter sw = File.CreateText(ConfigFolderPath + ConfigFile))
+                {
+                    sw.WriteLine(DataFolderPath);
+                }
+            }
+        }
+    }
+}
